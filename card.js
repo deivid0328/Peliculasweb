327b2e381d9e854da079e529fc68463134c337bd
Deivid Guauña <deividj.54@gmail.com>
2022-05-25 19:53:31 -0500

    generacion de popup

--- card.js
+++ card.js
@@ -6,7 +6,8 @@ const peliculas = [
         name: "Doctor Strange en el Multiverso de la Locura",
         title: "Doctor Strange in the Multiverse of Madness",
         estreno: "05-May-2022",
-        genero: "Acción, Aventura"
+        genero: "Acción, Aventura",
+        resumen:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident animi illo possimus, magnam, commodi earum voluptate ad expedita consequuntur nam eaque quibusdam nihil quos vitae minus quas itaque nemo minima?"
     },
     {
         id: 2,
@@ -15,7 +16,8 @@ const peliculas = [
         name: "El Peso del Talento",
         title: "The Unbearable Weight Of Massive Talent",
         estreno: "12-May-2022",
-        genero: "Acción, Comedia"
+        genero: "Acción, Comedia",
+        resumen:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident animi illo possimus, magnam, commodi earum voluptate ad expedita consequuntur nam eaque quibusdam nihil quos vitae minus quas itaque nemo minima?"
     },
     {
         id: 3,
@@ -24,7 +26,8 @@ const peliculas = [
         name: "Llamas de Venganza",
         title: "Firestarter",
         estreno: "12-May-2022",
-        genero: "Suspenso, Terror"
+        genero: "Suspenso, Terror",
+        resumen:"Loremt tipsum dolor sit amet, consectetur adipisicing elit. Provident animi illo possimus, magnam, commodi earum voluptate ad expedita consequuntur nam eaque quibusdam nihil quos vitae minus quas itaque nemo minima?"
     },
     {
         id: 4,
@@ -33,31 +36,87 @@ const peliculas = [
         name: "Sonic 2",
         title: "Sonic the Hedgehog 2",
         estreno: "07-Abr-2022",
-        genero: "Acción, Aventura"
+        genero: "Acción, Aventura",
+        resumen:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident animi illo possimus, magnam, commodi earum voluptate ad expedita consequuntur nam eaque quibusdam nihil quos vitae minus quas itaque nemo minima?"
     }
 ]
 const contenedor = document.getElementById("contenedor");
+const fragment= document.createDocumentFragment();
 
     peliculas.map((pelicula) => {
        
         const card = document.createElement("div");
         card.onclick = function(){
             if (pelicula.id == pelicula.id){
-                // window.location.href("./card.html")
-                const contenedorpeli = document.getElementById("peli");
-                const titulopeli = document.createElement("h1");
-                titulopeli.textContent=pelicula.name;
-                contenedorpeli.appendChild(titulopeli);
+               
+                const ventana=document.getElementById("ventana")
+                const popup = document.createElement('div')
+                popup.setAttribute('class','popup')
+                ventana.appendChild(popup)
+
+                const popup__contenedor=document.createElement("div")
+                popup__contenedor.setAttribute('class', 'popup__contenedor')
+                popup.appendChild(popup__contenedor)
+
+                const popup__info=document.createElement("div")
+                popup__info.setAttribute('class','popup__info')
+                popup__contenedor.appendChild(popup__info)
+
+                const popup__img=document.createElement("div")
+                popup__img.setAttribute('class', 'popup__img')
+                popup__info.appendChild(popup__img)
+
+                const img =document.createElement("img")
+                img.setAttribute('src', pelicula.img);
+                popup__img.appendChild(img)
+
+
+                const popup__descripcion= document.createElement('div')
+                popup__descripcion.setAttribute('class','popup__descripcion')
+                popup__info.appendChild(popup__descripcion)
+
+                const titulo = document.createElement("h3");
+                titulo.textContent=pelicula.name;
+                popup__descripcion.appendChild(titulo);
+
+                const ingles= document.createElement("span");
+                ingles.innerHTML=`<span><b>Titulo en ingles: </b>${pelicula.title}</span>`   
+                popup__descripcion.appendChild(ingles);
+
+                const estreno= document.createElement("span");
+                estreno.innerHTML=`<span><b>Estreno: </b>${pelicula.estreno}</span>`   
+                popup__descripcion.appendChild(estreno);
+
+                const genero= document.createElement("span");
+                genero.innerHTML=`<span><b>Género: </b>${pelicula.genero}</span>`   
+                popup__descripcion.appendChild(genero);
+    
+                const popup__sinopsis = document.createElement("div")
+                popup__sinopsis.setAttribute('class', 'popup__sinopsis')
+                popup__contenedor.appendChild(popup__sinopsis)
+
+                const sinopsis = document.createElement("h2")
+                sinopsis.textContent="Sinopsis"
+                popup__sinopsis.appendChild(sinopsis)
+
+                const parrafo=document.createElement("p")
+                parrafo.textContent=pelicula.resumen;
+                popup__sinopsis.appendChild(parrafo)
+
+
+
+
+
+
             }
         }
-        // function(event) { console.log(event.target.pelicula.id) };
-        // card.setAttribute('onclick', info());
+       
       
 
     card.setAttribute('id', pelicula.id);
     card.setAttribute('class', 'card__contenedor');
    
-    contenedor.appendChild(card);
+    fragment.appendChild(card);
 
     const cardimg = document.createElement("div")
     cardimg.setAttribute('class', 'card__img');
@@ -90,7 +149,6 @@ const contenedor = document.getElementById("contenedor");
 })
 
 
-function info(){
-console.log("copas")
+contenedor.appendChild(fragment)
 
-}
+
